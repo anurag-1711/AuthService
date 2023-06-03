29d14e72e3103b920708be85afdf52874897d203
Anurag <anurag171199@gmail.com>
2023-06-03 17:40:12 +0530

    Signup logic with password hashing done

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,30 @@
+const UserService = require("../services/user-service");
+
+const userService = new UserService();
+
+const create = async (req, res) => {
+    try {
+        const user = await userService.create({
+            email: req.body.email,
+            password: req.body.password
+        });
+        return res.status(201).json({
+            data: user,
+            message: "Succesfully created a new user",
+            success: true,
+            err: {}
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            message: "Something went wrong",
+            success: false,
+            err: error
+        })
+    }
+}
+
+module.exports = {
+    create
+}
